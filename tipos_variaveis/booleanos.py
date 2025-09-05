798e96f9d4de9da935a498e83357357e728e11af
Artur Moreira dos Santos <arturmoreira56dossantos@gmail.com>
2025-09-04 21:26:35 -0300

    .

---
+++ tipos_variaveis/booleanos.py
@@ -0,0 +1,44 @@
+#condição verdadeira
+True
+
+#condição falsa
+False 
+
+
+#Condicionais
+
+#se condição
+#Else = se não
+
+if True:
+    print("Bloco IF vai ser executado")
+else:
+    print("Bloco Else não sera executado")
+
+
+
+# Operadores lógicos: And e Or
+
+if True and True:
+    print("Bloco será executado")
+
+
+if True and False:
+    print("Bloco não será executado")
+
+
+if False and False:
+    print("Nada será executado")
+
+
+
+if True or False:
+    print("Or vai ser executado")
+
+if False or False:
+    print("Or não vai ser executado")
+
+
+if True or True:
+    print("Or vai ser executado")
+
---
+++ tipos_variaveis/lista.py
@@ -0,0 +1,10 @@
+#É uma coleção de elementos ordenaveis e mutaveis
+
+# Declaração de uma lista
+
+minha_lista = [1, 2, 3, 4, 5, "rocketseat", True, False]
+
+
+print("Minha lista de exemplo", minha_lista)
+
+
