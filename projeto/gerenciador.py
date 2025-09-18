e0d36f39b7f22b862e287611d50220b7c0ccf32e
Artur Moreira dos Santos <arturmoreira56dossantos@gmail.com>
2025-09-18 16:49:00 -0300

    .

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -1,3 +1,14 @@
+def adicionar_tarefa(tarefas, nome_tarefa):
+
+    # tarefa: nome da tarefa 
+    # completada: indicar se essa tarefa já foi completada ou não
+    tarefa = {"nome": nome_tarefa, "completada": False}
+    tarefas.append(tarefa)
+    print(f"Tarefa {nome_tarefa} foi adicionado com sucesso!")
+    return
+
+tarefas = []
+
 while True:
     print("\nMenu do gerenciador de tarefas")
     print("1. Adicionar uma tarefa")
@@ -7,9 +18,12 @@ while True:
     print("5. Deletar tarefas completadas")
     print("6. Sair")
 
-    escolha = input("Digite a sua escolha") 
+    escolha = input("Digite a sua escolha:") 
 
-    if escolha == "6": 
+    if escolha == "1":
+        nome_tarefa = input("Digite o nome da tarefa que deseja adicionar:")
+        adicionar_tarefa(tarefas, nome_tarefa)
+    elif escolha == "6": 
         break
 
 
