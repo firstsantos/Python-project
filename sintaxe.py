0f13e2873523e7c2f4a634dfd11ae190d40181d7
Artur Moreira dos Santos <arturmoreira56dossantos@gmail.com>
2025-09-23 09:27:17 -0300

    deletes

--- sintaxe.py
+++
@@ -1,9 +0,0 @@
-# Comentário é denomidado por #, algo que pode ser usado para registrar algo importante dentro do código
-
-"""Esse é meu comentário por aspas 
-   Dessa forma, pode ser listada mais de um paragrafo
-   enquanto estiver dentro das aspas.
-"""
- 
-nome = "Linu" + "Casemiro" #Essa é uma forma de fazer a junção de nomes
-
--- variaveis.py
+++
@@ -1,5 +0,0 @@
-nome_completo = "Gabriel Casemiro"
-idade = 25
-
-
-nomeCompleto = "Gabriel Casemiro"
